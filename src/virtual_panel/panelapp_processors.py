ce5bc04ef15b264372af40b552d876aea976556c
Hans Escobar <hansescobar@proton.me>
2025-08-08 11:48:53 -0500

    feat: add function to store conflicts for Status

--- src/virtual_panel/panelapp_processors.py
+++ src/virtual_panel/panelapp_processors.py
@@ -290,6 +290,24 @@ class PanelAppMerged:
         )
         logger.info("FIND CONFLICTS END.")
 
+    def save_status_conflicts(self, location: str) -> bool:
+        status_conflicts_df = self.conflicts.get("Status", None)
+
+        if status_conflicts_df is None:
+            logger.warning("There are not Status conflicts!")
+            return False
+
+        logger.info("Saving Status conflicts...")
+        status_conflicts_df[
+            [
+                "Name",
+                f"Status{self.suffix_left}",
+                f"Status{self.suffix_right}",
+            ]
+        ].to_feather(f"{location}.feather")
+        logger.info("Saving Status conflicts: Done!")
+        return True
+
     def make_consensus(
         self, custom_include: list[str], update_conflicts: bool = False
     ) -> pd.DataFrame:
