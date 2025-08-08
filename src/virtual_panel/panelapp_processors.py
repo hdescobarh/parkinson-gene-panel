5672f749bd2bc18ce9d7ca8c727a628ba50c0df0
Hans Escobar <hansescobar@proton.me>
2025-08-08 12:18:48 -0500

    refactor: reset indexes before saving in PanelAppMerge save_status_conflicts

--- src/virtual_panel/panelapp_processors.py
+++ src/virtual_panel/panelapp_processors.py
@@ -306,7 +306,7 @@ class PanelAppMerged:
                 f"Status{self.suffix_left}",
                 f"Status{self.suffix_right}",
             ]
-        ].to_feather(f"{dir}/{filename}.feather")
+        ].reset_index(drop=True).to_feather(f"{dir}/{filename}.feather")
         logger.info("Saving Status conflicts: Done!")
         return True
 
